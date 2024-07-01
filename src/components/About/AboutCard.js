941a60e38f6334c042365cefb2885e91500c691a
SaiTejaKolagani <saiteja.kolagani@gmail.com>
2024-07-01 09:58:51 +0000

    Initial Stage

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+import {ImPointRight} from 'react-icons/im'
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{textAlign: 'justify'}}>
+            Hi Everyone, I am <span className="purple">Sai Teja Kolagani </span>
+            from <span className="purple"> Tanuku, India.</span>
+            <br />
+            Currently, I am learning MERN Stack Development through Nxtwave's
+            4.0 Academic Program, with plans to dive into Java Full Stack
+            development.
+            <br />
+            I have completed my B.Tech in Electrical & Electronics Engineering
+            with a 71% score. My passion for Computer Science drives me to
+            explore digitalization technologies.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default AboutCard
---
+++ src/components/Footer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+import {AiFillGithub} from 'react-icons/ai'
+import {FaLinkedinIn} from 'react-icons/fa'
+import {FaSquareXTwitter} from 'react-icons/fa6'
+
+function Footer() {
+  let date = new Date()
+  let year = date.getFullYear()
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Sai Teja Kolagani</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} Sai Teja K</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/saiteja-kolagani"
+                style={{color: 'white'}}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://x.com/SaitejaKolagani"
+                style={{color: 'white'}}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaSquareXTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/sai-teja-kolagani/"
+                style={{color: 'white'}}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Footer
